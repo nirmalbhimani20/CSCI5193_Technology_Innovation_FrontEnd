99cc28155f64c5b237d9b3d6ac3cc73d3d524299
Nirmal Bhimani <nirbhimani@gmail.com>
2021-11-29 19:01:52 -0400

    credential change

--- src/services/instructor/instructor.service.ts
+++ src/services/instructor/instructor.service.ts
@@ -11,15 +11,15 @@ export class InstructorService {
 
   instructorList() {
     console.log("in instructor list method");
-    return this.httpClient.get<any>("http://localhost:3000/v1/user/instructorlist" );
+    return this.httpClient.get<any>("https://csci5193-nodejs.herokuapp.com/v1/user/instructorlist" );
   }
   checkUserIsPresent(JSON: any){
-    return this.httpClient.post<any>("http://localhost:3000/v1/user/checkUserIsPresent" , JSON);
+    return this.httpClient.post<any>("https://csci5193-nodejs.herokuapp.com/v1/user/checkUserIsPresent" , JSON);
   }
   addQuery(JSON: any){
-    return this.httpClient.post<any>("http://localhost:3000/v1/user/insertQueries" , JSON);
+    return this.httpClient.post<any>("https://csci5193-nodejs.herokuapp.com/v1/user/insertQueries" , JSON);
   }
   fetchQueries(JSON: any){
-    return this.httpClient.post<any>("http://localhost:3000/v1/user/fetchQueries" , JSON);
+    return this.httpClient.post<any>("https://csci5193-nodejs.herokuapp.com/v1/user/fetchQueries" , JSON);
   }
 }
