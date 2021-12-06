c1fcb6a1be633dfde5b87a314d9fbf33b9babe62
Thomas Rizzuto <ThomasRizzuto@dal.ca>
2021-12-06 11:10:52 -0400

    Update favicon, profile url

--- src/services/profile/profile.service.ts
+++ src/services/profile/profile.service.ts
@@ -10,12 +10,10 @@ export class ProfileService {
   constructor(private httpClient : HttpClient) { }
 
   getProfile(id: number) {
-    console.log("in instructor list method");
-    return this.httpClient.get<any>("http://localhost:3000/v1/user/profile", {params: {id: id}});
+    return this.httpClient.get<any>("https://csci5193-nodejs.herokuapp.com/v1/user/profile", {params: {id: id}});
   }
 
   updateProfile(id: number, name: string, email: string, about: string) {
-    console.log("in instructor list method");
-    return this.httpClient.post<any>("http://localhost:3000/v1/user/profile", {id: id, name: name, email: email, about: about});
+    return this.httpClient.post<any>("https://csci5193-nodejs.herokuapp.com/v1/user/profile", {id: id, name: name, email: email, about: about});
   }
 }
